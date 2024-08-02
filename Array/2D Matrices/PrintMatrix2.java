ba7b32fab97c892816422b288f8e7fc1e030130c
Prathamesh Mandhare <156640394+prathameshm2003@users.noreply.github.com>
2024-08-02 19:24:52 +0530

    Update PrintMatrix2.java

--- Array/2D Matrices/PrintMatrix2.java
+++ Array/2D Matrices/PrintMatrix2.java
@@ -7,8 +7,8 @@ public class PrintMatrix2 {
     public static void main(String[] args) {
         int arr[][] = new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
 
-        for (int i = 0; i < arr[0].length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+        for (int i = 0; i < arr[0].length; i++) { //arr[0].length=4
+            for (int j = 0; j < arr.length; j++) { //arr.length=3
                 System.out.print(arr[j][i] + " ");
 
             }
