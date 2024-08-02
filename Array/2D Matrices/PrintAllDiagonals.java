93ebdf8d430dc369e44eefeeda28db88ced9fbe4
Prathamesh Mandhare <156640394+prathameshm2003@users.noreply.github.com>
2024-08-02 20:39:06 +0530

    Update PrintAllDiagonals.java

--- Array/2D Matrices/PrintAllDiagonals.java
+++ Array/2D Matrices/PrintAllDiagonals.java
@@ -15,8 +15,8 @@ public class PrintAllDiagonals {
     public static void main(String[] args) {
         int arr[][] = new int[][] { { 1, 2, 3, 4, 5, 6 }, { 7, 8, 9, 10, 11, 12 }, { 13, 14, 15, 16, 17, 18 },
                 { 19, 20, 21, 22, 23, 24 }, { 25, 26, 27, 28, 29, 30 } };
-        int N = 5; 
-        int M = 6;
+        int N = 5; //rows
+        int M = 6; //columns
         for (int j = M - 1; j >= 0; j--) {
             int i = 0;
             int y = j;
