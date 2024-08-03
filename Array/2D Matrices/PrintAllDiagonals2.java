815871971b7d8bb3ae5e395055d6742ed7bf6245
Prathamesh Mandhare <156640394+prathameshm2003@users.noreply.github.com>
2024-08-03 10:11:54 +0530

    Add files via upload

---
+++ Array/2D Matrices/PrintAllDiagonals2.java
@@ -0,0 +1,33 @@
+package TwoDMatrices;
+
+/*
+ * Given a matrix of size N*M
+ * print all the diagonal (R->L)
+ * Note:- row==> 4th row
+ */
+public class PrintAllDiagonals2 {
+    public static void main(String[] args) {
+        int arr[][] = new int[][] {
+                { 1, 2, 3, 4, 5, 6 },
+                { 7, 8, 9, 10, 11, 12 },
+                { 13, 14, 15, 16, 17, 18 },
+                { 19, 20, 21, 22, 23, 24 },
+                { 25, 26, 27, 28, 29, 30 }
+        };
+        int N = 5;
+        int M = 6;
+
+        for (int j = M - 1; j >= 0; j--) {
+            int i = N - 1;
+            int y = j;
+
+            while (i >= 0 && y >= 0) {
+                System.out.print(arr[i][y] + " ");
+                i--;
+                y--;
+            }
+
+            System.out.println();
+        }
+    }
+}
